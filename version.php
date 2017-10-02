9b06b8ecb25afd180438ba02d577e895dcd2c4fa
Ponlawat <ponlawat_w@cmu.ac.th>
2017-10-02 08:23:38 +0700

    CMU BUS Version 1.1.5a
    - Change navbar color to dark gray

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $version = array(
-    'release' =>    '1.1.5',
-    'updated' =>    1505187000,
+    'release' =>    '1.1.5a',
+    'updated' =>    1506906000,
     'development'=> false
 );
