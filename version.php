3ae62fdb79b8c0a46db99d6f232fec19d6c69f83
Ponlawat <ponlawat_w@cmu.ac.th>
2018-02-02 11:30:37 +0700

    CMU BUS Version 1.2.1 release

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 $version = array(
-    'release' =>    '1.2.1',
-    'updated' =>    1517544000,
+    'release' => '1.2.1',
+    'updated' => 1517544000,
     'development'=> false,
-    'app_root' =>   'https://139.59.255.146/cmubus/app/'
+    'app_root' => 'https://139.59.255.146/cmubus/app/'
 );
